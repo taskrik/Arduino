1b289b3361a6d66b2c876b6e9fb5d74ddac4ec35
Tasos Krikonis <tasos.krikonis@gmail.com>
2019-04-04 23:14:51 +0200

    Checks if the time given matches the time now and opens the feeder

--- components/CatFeeder.js
+++ components/CatFeeder.js
@@ -1,6 +1,11 @@
 var five = require("johnny-five");
 var board = new five.Board();
+var moment = require("moment");
+
 const shell = require("shelljs");
+
+let feedingTimes = 0;
+let timeNow = moment().format("h:mm a");
 
 board.on("ready", function() {
   console.log("Auto Feeder is Ready!");
@@ -10,29 +15,43 @@ board.on("ready", function() {
   });
   const motor = new five.Servo({ pin: 11, startAt: 0 });
 
-  let feedingTimes = 0;
+    const startFeeding = (time, timeNow) => {
+    if (time === timeNow) {
+      activateFeeding();
+    } else {
+      console.log("Its not time yet!");
+      console.log("timeNow:", timeNow);
+      console.log("time: ", time);
+      setTimeout(() => startFeeding(time, timeNow = moment().format("h:mm a")), 20000);
+    }
+  };
 
   function activateFeeding() {
+    console.log("timeNow:", timeNow);
     feedingTimes += 1;
     led.blink(500);
     lcd.clear();
     motor.max();
-    setTimeout(() => {
-      motor.stop();
-      motor.home();
-      return;
-    }, 1000);
     lcd.cursor(0, 2).print("Fed Luke");
     lcd.cursor(1, 3).print(`${feedingTimes} times`);
-    shell.exec("./motionCaptures");
+    // shell.exec("./motionCaptures");
+    setTimeout(function stopFeeding() {
+      motor.stop();
+      motor.home();
+      led.stop();
+      led.off();
+      lcd.cursor(0, 2).print("Fed Luke");
+      lcd.cursor(1, 3).print(`${feedingTimes} times`);
+    }, 3000);
   }
 
-  activateFeeding();
+  startFeeding("11:13 pm", timeNow = moment().format("h:mm a"));
 
   this.on("exit", function() {
     led.stop();
     led.off();
     lcd.off();
     motor.stop();
+    motor.home();
   });
 });
