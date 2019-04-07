1c3dff7f3dc613183d0b6e133a1695a14e560dbe
Tasos Krikonis <tasos.krikonis@gmail.com>
2019-04-07 21:52:22 +0200

    Updates the server

--- server/app.js
+++ server/app.js
@@ -6,14 +6,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
+console.log(path.join(__dirname, "public"));
+
 let feedingTimes = 0;
 
 board.on("ready", function() {
   app.get("/", (req, res) => {
-    res.send("Dashboard main page!")
+    motor.stop();
     motor.home();
     led.stop();
     led.off();
+    return res.render("index", { status: "Press Button To Feed Luke !!" });
   });
 
   const led = new five.Leds([13, 6, 5, 4]);
@@ -23,22 +30,21 @@ board.on("ready", function() {
   const motor = new five.Servo({ pin: 11, startAt: 0 });
   console.log("Auto Feeder is Ready!");
 
-  app.get("/feeding/on", function(req, res) {
-    res.send("Activating feeder");
+  app.post("/feeding/on", function(req, res) {
     console.log("Feeder opened");
-    
+
     feedingTimes += 1;
     led.blink(500);
     lcd.clear();
     motor.max();
     lcd.cursor(0, 2).print("Fed Luke");
     lcd.cursor(1, 3).print(`${feedingTimes} times`);
+    return res.render("index", { status: "Feeder opened" });
   });
 
-  app.get("/feeding/off", function(req, res) {
-    res.send("Closing feeder");
+  app.post("/feeding/off", function(req, res) {
     console.log("Feeder closed");
-    
+
     // shell.exec("./motionCaptures");
     motor.stop();
     motor.home();
@@ -46,6 +52,7 @@ board.on("ready", function() {
     led.off();
     lcd.cursor(0, 2).print("Fed Luke");
     lcd.cursor(1, 3).print(`${feedingTimes} times`);
+    return res.render("index", { status: "Feeder closed" });
   });
 });
 
