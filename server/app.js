8c47f584d234295e776ceded767b257f4ad6d4e0
Tasos Krikonis <tasos.krikonis@gmail.com>
2019-04-07 19:34:24 +0200

    Creates an express server

---
+++ server/app.js
@@ -0,0 +1,14 @@
+import StartFeeding from "./ActivateFeeder";
+
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Dashboard main page!"));
+
+app.post("/feeding/on", function(req, res) {
+  StartFeeding();
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
