14f706e8aff29755f3265accf86636848ec41811
Tasos Krikonis <tasos.krikonis@gmail.com>
2019-06-02 16:44:52 +0200

    Creates a testing server for new implementation

--- server/app2.js
+++ server/app2.js
@@ -7,11 +7,13 @@ const app = express();
 const port = 3000;
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+
 const Raspi = require("raspi-io").RaspiIO;
 
 const board = new five.Board({
   io: new Raspi()
 });
+
 let feedingTimes = 0;
 let timeStamps = [];
 let countFramesSent = 0;
@@ -22,11 +24,14 @@ wCap.set(cv.CAP_PROP_FRAME_HEIGHT, 300);
 wCap.set(cv.CAP_PROP_FRAME_WIDTH, 300);
 
 board.on("ready", function() {
-  const led = new five.Leds([13, 6, 5, 4]);
-  const lcd = new five.LCD({
-    controller: "PCF8574A"
+  //   const lcd = new five.LCD({
+  //     controller: "PCF8574A"
+  //   });
+  const motor = new five.Servo({
+    pin: "GPIO10",
+    pwmRange: [600, 2600],
+    startAt: 0
   });
-  const motor = new five.Servo({ pin: 'GPIO27', startAt: 0 });
   console.log("Auto Feeder is Ready!");
 
   //gets feeder's status
@@ -44,11 +49,10 @@ board.on("ready", function() {
 
     feedingTimes += 1;
     timeStamps.unshift(moment().format("h:mm:ss a"));
-    led.blink(500);
-    lcd.clear();
+    // lcd.clear();
     motor.max();
-    lcd.cursor(0, 2).print("Fed Luke");
-    lcd.cursor(1, 3).print(`${feedingTimes} times`);
+    // lcd.cursor(0, 2).print("Fed Luke");
+    // lcd.cursor(1, 3).print(`${feedingTimes} times`);
     return res.json({
       message: "Feeder is open!",
       feederInfo: { timesUsed: feedingTimes, hours: timeStamps }
@@ -61,10 +65,8 @@ board.on("ready", function() {
 
     motor.stop();
     motor.home();
-    led.stop();
-    led.off();
-    lcd.cursor(0, 2).print("Fed Luke");
-    lcd.cursor(1, 3).print(`${feedingTimes} times`);
+    // lcd.cursor(0, 2).print("Fed Luke");
+    // lcd.cursor(1, 3).print(`${feedingTimes} times`);
     return res.json({
       message: "Feeder closed!",
       feederInfo: { timesUsed: feedingTimes, hours: timeStamps }
@@ -77,8 +79,8 @@ board.on("ready", function() {
 
     feedingTimes = 0;
     timeStamps = [];
-    lcd.cursor(0, 2).print("Fed Luke");
-    lcd.cursor(1, 3).print(`${feedingTimes} times`);
+    // lcd.cursor(0, 2).print("Fed Luke");
+    // lcd.cursor(1, 3).print(`${feedingTimes} times`);
     return res.json({
       message: "Counter reset successfully",
       feederInfo: { timesUsed: feedingTimes, hours: timeStamps }
